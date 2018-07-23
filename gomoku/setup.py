a6cd10675cd3349cfb50a5353b4d4e9d0e9b8f43
Seokho Hong <hseokho@cisco.com>
2018-07-23 07:59:50 -0700

    fixed bugs with board

--- gomoku/setup.py
+++ gomoku/setup.py
@@ -4,13 +4,9 @@ from setuptools import setup, find_packages
  
  
 setup(
-    name="gomoku",
-    author="Seokho Hong",
-    author_email="hseokho@cisco.com",
-    url="https://wwwin-github.cisco.com/GVS-CS-DSX/package_repo",
+    name="gomoku2",
     python_requires=">3",
     install_requires=[],
-    package_directory={"", "src"},
     include_package_data=True,
-    packages=['gomoku'],
+    packages=['core', 'learner', 'main', 'util'],
 )
