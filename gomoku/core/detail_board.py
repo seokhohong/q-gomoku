c7e2de0d13aaf4c4ecdeca4c629779bc0a80f21c
Seokho Hong <hseokho@cisco.com>
2018-07-24 15:50:52 -0700

    highly detailed board

--- gomoku/core/detail_board.py
+++ gomoku/core/detail_board.py
@@ -25,6 +25,8 @@ class Board:
     SECOND_PLAYER = -1
 
     CHAIN_DIRECTIONS = 4
+    NUM_PLAYERS = 2
+
     def __init__(self, size=5, win_chain_length=3):
         self.size = size
         # three channels
@@ -46,8 +48,33 @@ class Board:
 
         # memory of how each move changed the chain lengths
         self.chain_length_memory = []
-        self.chain_matrix = np.zeros((self.size, self.size, Board.CHAIN_DIRECTIONS))
-        self.chain_matrix.fill(Board.NO_PLAYER)
+
+        # memory of blocked directions
+        self.chain_block_memory = []
+
+        # how many of the same stones are strung together
+        # stores an integer
+        self.chain_matrix = np.zeros((self.size, self.size, Board.NUM_PLAYERS, Board.CHAIN_DIRECTIONS))
+        self.chain_matrix.fill(0)
+
+        # whether the chain in a particular direction is blocked
+        # 1 indicates free on both, 0 indicates blocked on one side, -1 indicates blocked on both
+        self.chain_block_matrix = np.zeros((self.size, self.size, Board.NUM_PLAYERS, Board.CHAIN_DIRECTIONS))
+        self.chain_block_matrix.fill(1)
+
+        class Direction:
+            def __init__(self, x, y, index):
+                self.x = x
+                self.y = y
+                self.index = index
+
+        # direction mapping
+        self.directions = {
+            'HORIZONTAL': Direction(-1, 0, 0),
+            'SLASH': Direction(-1, 1, 1),
+            'BACKSLASH': Direction(1, 1, 2),
+            'VERTICAL': Direction(0, 1, 3)
+        }
 
         # whether current game_state is accurate
         self.state_computed = False
@@ -63,9 +90,11 @@ class Board:
         self.matrix[x, y, self.player_to_move] = 1
         self.matrix[x, y, self.NO_PLAYER] = 0
         self.chain_length_memory.append(np.copy(self.chain_matrix))
+        self.chain_block_memory.append(np.copy(self.chain_block_matrix))
 
         self.available_moves.remove((x, y))
         self.compute_game_state()
+        self.update_chains(x, y)
 
         self.flip_player_to_move()
 
@@ -75,8 +104,8 @@ class Board:
         self.matrix[previous_move.x, previous_move.y, previous_move.player] = 0
         self.matrix[previous_move.x, previous_move.y, Board.NO_PLAYER] = 1
 
-        previous_chain = self.chain_length_memory.pop()
-        self.chain_matrix = previous_chain
+        self.chain_matrix = self.chain_length_memory.pop()
+        self.chain_block_matrix = self.chain_block_memory.pop()
 
         self.available_moves.add((previous_move.x, previous_move.y))
         self.flip_player_to_move()
@@ -90,7 +119,15 @@ class Board:
 
         matrix = -np.copy(self.matrix)
 
-        return np.concatenate((matrix, np.copy(self.chain_matrix).reshape(self.size, self.size, Board.CHAIN_DIRECTIONS)), axis=2)
+        # stack the two chain matrices so that index [:, :, 0, :] is self and [:, :, 1, :] is other
+        if as_player == Board.FIRST_PLAYER:
+            chains = np.concatenate((self.chain_matrix[:, :, 0, :], self.chain_matrix[:, :, 1, :]), axis=2)
+            blocked = np.concatenate((self.chain_block_matrix[:, :, 0, :], self.chain_block_matrix[:, :, 1, :]), axis=2)
+        else:
+            chains = np.concatenate((self.chain_matrix[:, :, 1, :], self.chain_matrix[:, :, 0, :]), axis=2)
+            blocked = np.concatenate((self.chain_block_matrix[:, :, 1, :], self.chain_block_matrix[:, :, 0, :]), axis=2)
+
+        return np.concatenate((matrix, chains, blocked), axis=2)
 
 
     def get_rotated_matrices(self, as_player):
@@ -145,10 +182,10 @@ class Board:
         if last_move:
             last_x, last_y = last_move.x, last_move.y
             # check win
-            if self.chain_length(last_x, last_y, -1, 0, last_move.player, 0) >= self.win_chain_length\
-                    or self.chain_length(last_x, last_y, -1, 1, last_move.player, 1) >= self.win_chain_length \
-                    or self.chain_length(last_x, last_y, 1, 1, last_move.player, 2) >= self.win_chain_length \
-                    or self.chain_length(last_x, last_y, 0, 1, last_move.player, 3) >= self.win_chain_length:
+            if self.chain_length(last_x, last_y, self.directions['HORIZONTAL']) >= self.win_chain_length\
+                    or self.chain_length(last_x, last_y, self.directions['SLASH']) >= self.win_chain_length \
+                    or self.chain_length(last_x, last_y, self.directions['BACKSLASH']) >= self.win_chain_length \
+                    or self.chain_length(last_x, last_y, self.directions['VERTICAL']) >= self.win_chain_length:
                     self.game_state = GameState.WON
                     return
             if len(self.ops) == self.size ** 2:
@@ -160,36 +197,93 @@ class Board:
     def in_bounds(self, x, y):
         return 0 <= x < self.size and 0 <= y < self.size
 
-    # does both directions
-    def chain_length(self, center_x, center_y, delta_x, delta_y, center_stone, direction):
-        # other spots other than center that are part of the chain
-        chain_positions = [(center_x, center_y)]
-        if center_stone == Board.NO_PLAYER:
-            return 0
-        chain_length = 1
+    def center_stone(self, x, y):
+        if abs(self.matrix[x, y, 1] - 1) < 1E-6:
+            return Board.FIRST_PLAYER
+        if abs(self.matrix[x, y, 2] - 1) < 1E-6:
+            return Board.SECOND_PLAYER
+        return Board.NO_PLAYER
+
+    # search in all directions from move made and update chain lengths there
+    def update_chains(self, center_x, center_y):
+        # for center only
+        for key, chain_dir in self.directions.items():
+            self.chain_length(center_x, center_y, chain_dir)
+        # walking direction
+        for direction in self.directions.values():
+            for step in range(1, self.size):
+                step_x = direction.x * step
+                step_y = direction.y * step
+                # stepped x, y
+                this_x = center_x + step_x
+                this_y = center_y + step_y
+                # search in all directions and update
+                if 0 <= this_x < self.size and 0 <= this_y < self.size:
+                    # chain check direction
+                    self.chain_length(this_x, this_y, direction)
+                else:
+                    break
+
+        for direction in self.directions.values():
+            for step in range(1, self.size):
+                step_x = direction.x * step * -1
+                step_y = direction.y * step * -1
+                # stepped x, y
+                this_x = center_x + step_x
+                this_y = center_y + step_y
+                # search in all directions and update
+                if 0 <= this_x < self.size and 0 <= this_y < self.size:
+                    # chain check direction
+                    self.chain_length(this_x, this_y, direction)
+                else:
+                    break
+
+    def directional_chain_length(self, center_x, center_y, delta_x, delta_y, center_stone, chain_positions):
+        chain_length = 0
+
+        blocked = 1
         for step in range(1, self.win_chain_length):
             step_x = delta_x * step
             step_y = delta_y * step
-            if 0 <= center_x + step_x < self.size and 0 <= center_y + step_y < self.size and \
-                    self.matrix[center_x + step_x, center_y + step_y, center_stone] == 1:
-                chain_length += 1
-                chain_positions.append([center_x + step_x, center_y + step_y])
-            else:
-                break
-        # other direction
-        for step in range(1, self.win_chain_length):
-            step_x = -delta_x * step
-            step_y = -delta_y * step
-            if 0 <= center_x + step_x < self.size and 0 <= center_y + step_y < self.size and \
-                    self.matrix[center_x + step_x, center_y + step_y, center_stone] == 1:
-                chain_length += 1
-                chain_positions.append([center_x + step_x, center_y + step_y])
+            # stepped x, y
+            this_x = center_x + step_x
+            this_y = center_y + step_y
+            if 0 <= this_x < self.size and 0 <= this_y < self.size:
+                if self.matrix[this_x, this_y, center_stone] == 1:
+                    chain_length += 1
+                    chain_positions.append([this_x, this_y])
+                elif self.matrix[this_x, this_y, Board.NO_PLAYER] == 1:
+                    blocked = 0
+                    break
+                else:
+                    break
             else:
                 break
 
+        return chain_length, blocked
+
+    # does both directions
+    def chain_length(self, center_x, center_y, direction):
+        # other spots other than center that are part of the chain
+        delta_x, delta_y = direction.x, direction.y
+        chain_positions = [(center_x, center_y)]
+
+        center_stone = self.center_stone(center_x, center_y)
+        chain_player = 0 if center_stone == 1 else 1
+
+        if center_stone == Board.NO_PLAYER:
+            return 0
+
+        chain_length_1, blocked_1 = self.directional_chain_length(center_x, center_y, delta_x, delta_y, center_stone, chain_positions)
+        chain_length_2, blocked_2 = self.directional_chain_length(center_x, center_y, -delta_x, -delta_y, center_stone, chain_positions)
+        chain_length = chain_length_1 + chain_length_2 + 1
+
+        # blocked math is funky, but works out to produce an indicator
+        self.chain_block_matrix[center_x, center_y, chain_player, direction.index] = -(blocked_1 + blocked_2) + 1
+
         for x, y in chain_positions:
-            if chain_length > self.chain_matrix[x, y, direction]:
-                self.chain_matrix[x, y, direction] = chain_length
+            if chain_length > self.chain_matrix[x, y, chain_player, direction.index]:
+                self.chain_matrix[x, y, chain_player, direction.index] = chain_length
 
         return chain_length
 
